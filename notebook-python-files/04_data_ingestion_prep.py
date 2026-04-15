e8108bd34449a29064240e0d14a3cf3cb420d39a
Gabriel Oduor <91619174+TechAriel@users.noreply.github.com>
2026-04-15 18:14:08 +0200

    Update 04_data_ingestion_prep.py

--- notebook-python-files/04_data_ingestion_prep.py
+++ notebook-python-files/04_data_ingestion_prep.py
@@ -1,46 +1,24 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# ## Data Ingestion Notebook Prep
-# 
-# New notebook
-
-# In[1]:
+# Data Ingestion Notebook Prep
 
 
 # API url for source data (range from 01/01/2014 to 02/01/2014)
 url = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2014-01-01&endtime=2014-01-02"
 
 
-# In[3]:
-
-
 # import requests module to retrive data from source url
 import requests
-
-
-# In[8]:
 
 
 # status code 200 means the request is successfull
 requests.get(url).status_code
 # assign request to a variable "response"
-
 response = requests.get(url)
 
 
-# In[9]:
-
-
 # return response in json format
-
 response.json()
 
 
-# In[11]:
-
-
 # accessing data from the features key
-
 response.json()['features']
 
